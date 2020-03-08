a8f394533f95d483a811179f8039fbcfce0d288c
ChickenPollo <luimarti@student.42.us.org>
2020-03-07 21:53:20 -0800

    added header

---
+++ fillit.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fillit.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: luimarti <luimarti@student.42.us.org>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/03/07 21:49:44 by luimarti          #+#    #+#             */
+/*   Updated: 2020/03/07 21:52:51 by luimarti         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FILLIT_H
+# define FILLIT_H
+
+# define MAP_MAX 16
+
+typedef struct s_tetris
+{
+	char			t[4][4];
+	struct s_tetris	*next;
+}				t_tetris;
+
+void		exit_error(int i);
+t_tetris	*parse_data(char *file_name);
+
+#endif
