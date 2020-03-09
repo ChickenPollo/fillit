cf7c4f54ecdeec33c5dcb11306e1defc409bbd83
Felix Jankowski <fjankows@student.42.us.org>
2020-03-08 19:51:54 -0700

    add a first working solution and some test files

--- fillit.h
+++ fillit.h
@@ -3,17 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   fillit.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: luimarti <luimarti@student.42.us.org>      +#+  +:+       +#+        */
+/*   By: fjankows <fjankows@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/07 21:49:44 by luimarti          #+#    #+#             */
-/*   Updated: 2020/03/08 18:44:41 by luimarti         ###   ########.fr       */
+/*   Updated: 2020/03/08 19:47:04 by fjankows         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FILLIT_H
 # define FILLIT_H
 
-# define MAP_MAX 16
+# define MAP_MAX 64
 
 typedef struct	s_tetris
 {
@@ -23,6 +23,8 @@ typedef struct	s_tetris
 }				t_tetris;
 
 void			exit_error(int i);
+void			print_map(char map[][MAP_MAX], int map_dim);
+
 t_tetris		*parse_data(char *file_name);
 
 #endif
--- utils.c
+++ utils.c
@@ -3,15 +3,37 @@
 /*                                                        :::      ::::::::   */
 /*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: luimarti <luimarti@student.42.us.org>      +#+  +:+       +#+        */
+/*   By: fjankows <fjankows@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/08 17:28:21 by luimarti          #+#    #+#             */
-/*   Updated: 2020/03/08 17:29:23 by luimarti         ###   ########.fr       */
+/*   Updated: 2020/03/08 19:20:24 by fjankows         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 #include "libft/libft.h"
+
+void		print_map(char map[][MAP_MAX], int map_dim)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (i < map_dim)
+	{
+		j = 0;
+		while (j < map_dim)
+		{
+			if (map[i][j])
+				ft_putchar(map[i][j]);
+			else
+				ft_putchar('.');
+			++j;
+		}
+		ft_putchar('\n');
+		++i;
+	}
+}
 
 void		exit_error(int i)
 {
